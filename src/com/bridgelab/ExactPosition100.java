c81d782bc897a7b71c34a3d684573760858f77ea
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-06 16:54:30 +0530

    UC5

---
+++ src/com/bridgelab/ExactPosition100.java
@@ -0,0 +1,48 @@
+package com.bridgelab;
+
+public class ExactPosition100 {
+    public static void main(String[] args) {
+        System.out.println("Player Exact Winning Position is: ");
+
+//		constants for program
+        int WINNING_POSITION = 100;
+        int position = 0; // Initializing Variable
+
+        // while loop use to execute Condition
+        while (position < WINNING_POSITION) {
+
+            // to roll a dice
+            int diceNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println("DiceNumber is : " + diceNumber);
+
+            // check option after getting a diceNumber
+            int option = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Selected player option : " + option);
+
+            switch (option) {
+                case 1:
+                    position = diceNumber + position;
+                    System.out.println("Ladder");
+                    if (position > 100) {
+                        position = 100;
+                        position = position - diceNumber;
+                    }
+                    System.out.println("Position for the player after the ladder is : " + position);
+                    break;
+
+                case 2:
+                    position = position - diceNumber;
+                    System.out.println("Snack");
+                    if (position < 0) {
+                        position = 0;
+                        System.out.println("Player restart from zero: ");
+                    }
+                    System.out.println("Position for the player after the Snake is : " + position);
+                    break;
+
+                default:
+                    System.out.println("No play");
+            }
+        }
+    }
+}
