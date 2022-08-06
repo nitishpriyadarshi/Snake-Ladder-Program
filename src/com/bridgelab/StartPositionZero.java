91fe42269d059dd7761b2969594b417c73f16159
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-06 16:15:43 +0530

    first commit

---
+++ src/com/bridgelab/StartPositionZero.java
@@ -0,0 +1,9 @@
+package com.bridgelab;
+
+public class StartPositionZero {
+    public static void main(String[] args) {
+        // starting position of player
+        int startPosition = 0;
+        System.out.println(" Start position is: " + startPosition);
+    }
+}
