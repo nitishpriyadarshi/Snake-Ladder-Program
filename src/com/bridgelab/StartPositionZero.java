eb51095eee0ab99ae60bd3ea89bd887180cc3ebb
nitishpriyadarshi <nitish8152@gmail.com>
2022-08-06 16:23:26 +0530

    welcome message

--- src/com/bridgelab/StartPositionZero.java
+++
@@ -1,9 +0,0 @@
-package com.bridgelab;
-
-public class StartPositionZero {
-    public static void main(String[] args) {
-        // starting position of player
-        int startPosition = 0;
-        System.out.println(" Start position is: " + startPosition);
-    }
-}
---
+++ src/com/bridgelab/Welcome.java
@@ -0,0 +1,7 @@
+package com.bridgelab;
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("---Welcome to snake Ladder Program---");
+    }
+}
